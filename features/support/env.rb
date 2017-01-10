c1126c7b566f7856706a32f48f2700c7da0e6885
Stephen Davidson <shdavidson90@gmail.com>
2017-01-10 12:34:44 +0100

    update deps and change default browser to chrome

--- features/support/env.rb
+++ features/support/env.rb
@@ -13,7 +13,7 @@ config = InstanceCreator.config
 Capybara.app_host = config['url']
 
 Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, :browser => (ENV['BROWSER'] ||= 'firefox').to_sym)
+  Capybara::Selenium::Driver.new(app, :browser => (ENV['BROWSER'] ||= 'chrome').to_sym)
 end
 
 Capybara.default_driver = :selenium
